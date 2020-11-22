b26d8e6653bd501ca97df4280101df7406b97820
modios <modp3rl@gmail.com>
2020-11-22 02:40:52 +0000

    Add get by country query

--- src/graphql_infrastructure/graphql_queries.py
+++ src/graphql_infrastructure/graphql_queries.py
@@ -1,4 +1,4 @@
-from graphene import ObjectType,Field,List
+from graphene import ObjectType,Field,List, String
 from graphql_infrastructure.graphql_types import CovidInfoByCountryType, CovidInfoType 
 from database.models import CovidInfoByCountry;
    
@@ -7,3 +7,20 @@ class GetAllCountriesQuery(ObjectType):
 
      def resolve_get_all_countries_covid_info(root, info): 
             return list(map(lambda x: CovidInfoByCountryType.model_to_type(x), CovidInfoByCountry.objects.all()))
+
+
+class JohnsHopkinsCSSE(ObjectType):
+     all_countries = Field(GetAllCountriesQuery)
+     covid_info_by_country = List(CovidInfoType, countryName = String())
+     def resolve_all_countries(root, info):
+          return {}
+     
+     def resolve_covid_info_by_country(root, info, countryName):
+          return list(CovidInfoByCountry.objects(country=countryName).first().covidInfo)
+
+
+class RootQuery(ObjectType):
+     JohnsHopkinsCSSE = Field(JohnsHopkinsCSSE)
+
+     def resolve_JohnsHopkinsCSSE(root, info):
+          return {}
