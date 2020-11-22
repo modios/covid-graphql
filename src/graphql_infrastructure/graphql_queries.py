438cf7c765da86a56480139fd9c9016eeca2fa19
modios <modp3rl@gmail.com>
2020-11-22 20:26:06 +0000

    Add graphql types for Npho Objects

--- src/graphql_infrastructure/graphql_queries.py
+++ src/graphql_infrastructure/graphql_queries.py
@@ -1,6 +1,7 @@
 from graphene import ObjectType,Field,List, String
-from graphql_infrastructure.graphql_types import CovidInfoByCountryType, CovidInfoType 
+from graphql_infrastructure.graphql_types import CovidInfoByCountryType, CovidInfoType,NphoIntensiveCareCasesType, NphoTotalTestsType
 from database.models import CovidInfoByCountry;
+from database.models import NphoIntensiveCareCases, NphoTotalTests
    
 class GetAllCountriesQuery(ObjectType):
      get_all_countries_covid_info = Field(List(CovidInfoByCountryType))
@@ -18,9 +19,20 @@ class JohnsHopkinsCSSE(ObjectType):
      def resolve_covid_info_by_country(root, info, countryName):
           return list(CovidInfoByCountry.objects(country=countryName).first().covidInfo)
 
+class Npho(ObjectType):
+     intes_care_cases = List(NphoIntensiveCareCasesType)
+     total_tests = List(NphoTotalTestsType)
+     def resolve_intes_care_cases(root, info):
+          return  list(map(lambda x: NphoIntensiveCareCasesType.model_to_type(x), NphoIntensiveCareCases.objects.all()))
+
+     def resolve_total_tests(root, info):
+          return  list(map(lambda x: NphoTotalTestsType.model_to_type(x), NphoTotalTests.objects.all()))
 
 class RootQuery(ObjectType):
      JohnsHopkinsCSSE = Field(JohnsHopkinsCSSE)
+     Npho = Field(Npho)
 
      def resolve_JohnsHopkinsCSSE(root, info):
+          return {}
+     def resolve_Npho(root, info):
           return {}
--- src/graphql_infrastructure/graphql_types.py
+++ src/graphql_infrastructure/graphql_types.py
@@ -2,7 +2,7 @@ from graphene import ObjectType,Field
 from database.models import CovidInfoByCountry
 from database.models import CovidInfo
 import graphene
-
+from database.models import NphoIntensiveCareCases, NphoTotalTests
 
 class CovidInfoType(ObjectType):
     date = graphene.String()
@@ -25,6 +25,25 @@ class CovidInfoByCountryType(ObjectType):
     def model_to_type(covidByCountry : CovidInfoByCountry):
        return CovidInfoByCountryType(
        country = covidByCountry.country,
-       covidInfo = list(map(lambda x: CovidInfoType.model_to_type(x), covidByCountry.covidInfo))
-       )
+       covidInfo = list(map(lambda x: CovidInfoType.model_to_type(x), covidByCountry.covidInfo)))
 
+class NphoTotalTestsType(ObjectType):
+    date =graphene.String()
+    rapid_tests = graphene.Int()
+    tests = graphene.Int()
+    @staticmethod
+    def model_to_type(nphoTotalTests : NphoTotalTests):
+        return NphoTotalTestsType(
+            date = nphoTotalTests.date,
+            rapid_tests = nphoTotalTests.rapid_tests,
+            tests = nphoTotalTests.tests)
+
+class NphoIntensiveCareCasesType(ObjectType):
+    date = graphene.String()
+    intensive_care =  graphene.Int()
+
+    @staticmethod
+    def model_to_type(nphoIntensiveCareCases : NphoIntensiveCareCases):
+       return NphoIntensiveCareCasesType(
+            date = nphoIntensiveCareCases.date,
+            intensive_care = nphoIntensiveCareCases.intensive_care)
